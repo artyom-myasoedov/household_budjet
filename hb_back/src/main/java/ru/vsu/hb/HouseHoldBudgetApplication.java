518c4abdd634d4bb11dbb06be42dc946ea27f733
Artyom <volot36rus@gmail.com>
2022-04-03 21:10:40 +0300

    HB-16: init gradle dependencies

---
+++ hb_back/src/main/java/ru/vsu/hb/HouseHoldBudgetApplication.java
@@ -0,0 +1,12 @@
+package ru.vsu.hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHoldBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseHoldBudgetApplication.class, args);
+    }
+}
