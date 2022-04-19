132ce5da9e43c42c4a567e0c0ef4185d4d7d3223
Artyom <volot36rus@gmail.com>
2022-04-20 00:02:44 +0300

    HB-39: fix controller responses

---
+++ hb_back/src/main/java/ru/vsu/hb/utils/HBResponseBuilder.java
@@ -0,0 +1,32 @@
+package ru.vsu.hb.utils;
+
+import com.leakyabstractions.result.Result;
+import org.springframework.http.ResponseEntity;
+import ru.vsu.hb.dto.error.HBError;
+import ru.vsu.hb.dto.response.HBResponseData;
+
+import static ru.vsu.hb.security.SecurityConstants.HEADER_STRING;
+
+public class HBResponseBuilder<T> {
+
+    private ResponseEntity<? super HBResponseData<? super T>> response;
+
+    public HBResponseBuilder(ResponseEntity<? super HBResponseData<? super T>> response) {
+        this.response = response;
+    }
+
+    public static <T> HBResponseBuilder<T> fromHBResult(Result<T, HBError> result) {
+        return new HBResponseBuilder<>(ControllerUtils.toHBResult(result));
+    }
+
+    public HBResponseBuilder<T> withAuthToken(String token) {
+        response = ResponseEntity.status(response.getStatusCode())
+                .header(HEADER_STRING, token)
+                .body(response.getBody());
+        return this;
+    }
+
+    public ResponseEntity<? super HBResponseData<? super T>> build() {
+        return response;
+    }
+}
