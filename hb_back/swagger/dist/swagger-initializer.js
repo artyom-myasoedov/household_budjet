54b5171a0780de0e11b3cf69e9ec8385fa366799
Artyom Myasoedov <71248582+artyom-myasoedov@users.noreply.github.com>
2022-04-25 22:47:37 +0300

    Update swagger-initializer.js

--- hb_back/swagger/dist/swagger-initializer.js
+++ hb_back/swagger/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "hb_back/swagger/swagger.json",
+    url: "https://artyom-myasoedov.github.io/household_budjet/hb_back/swagger/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
